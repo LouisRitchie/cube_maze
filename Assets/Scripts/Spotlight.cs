bf3346ce0d96c45554dd7da4d3d25ffa72ee0241
gregolopogus <gregoryperry627@gmail.com>
2017-03-07 21:03:11 -0800

    Some textures, and a big light
    Added a couple blocks and some ground and threw some concrete textures
    on them. Got a nifty skybox for the nightime and made a big tower that
    shines a light down. I was trying to get the tower to randomly scan
    around but the script is super buggy. Also I was playing with the
    ambient light and stuff and the low lighting makes the concrete textures
    look really oily, but that can be fixed.

---
+++ Assets/Scripts/Spotlight.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spotlight : MonoBehaviour {
+    public Light spotlight;
+    public int xrot = 115, yrot = 83, zrot = 161;
+    // Use this for initialization
+    public Transform startMarker;
+    public Transform endMarker;
+    public float speed = 0.1f;
+    private float startTime;
+    private float journeyLength;
+    Quaternion startAngle;
+    Quaternion endAngle;
+
+    void Start() {
+        startAngle = Quaternion.Euler(xrot,yrot,zrot);
+        endAngle = Quaternion.Euler(xrot + Random.Range(-10, 10), yrot + Random.Range(-10, 10), zrot + Random.Range(-10, 10));
+
+    }
+    void Update() {
+        spotlight.transform.rotation = Quaternion.Slerp(startAngle,endAngle,Time.time*speed);
+        startAngle = endAngle;
+        endAngle = Quaternion.Euler(xrot + Random.Range(-10, 10), yrot + Random.Range(-10, 10), zrot + Random.Range(-10, 10));
+    }
+}
+
