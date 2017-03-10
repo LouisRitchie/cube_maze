8250377858af98654d3beb40dee8bf0c2109a9bf
twistedburger <twistedburger@hotmail.com>
2017-03-09 21:13:50 -0800

    ending

---
+++ Assets/Scripts/Ending.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ending : MonoBehaviour {
+    private bool isAtEnd = false;
+
+    
+    void OnTriggerEnter(Collider interactor) {
+        print("collided");
+        if (interactor.tag == "Player") {
+            print("press E");
+            isAtEnd=true;
+        }
+    }
+
+    void OnTriggerExit(Collider interactor) {
+        if (interactor.tag == "Player") {
+            isAtEnd=false;
+            print("not close enough");
+        }
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.E)) {
+            if (isAtEnd) {
+           
+            print("You win you frekkin nerd!");
+                }
+            Time.timeScale = 0;
+        }
+
+
+    }
+
+
+
+
+}
