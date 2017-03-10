238640c197c93d1b5c82242f15ccbb935dd87055
twistedburger <twistedburger@hotmail.com>
2017-03-09 21:50:45 -0800

    MELTTFY

---
+++ Assets/Scripts/MELTTFY.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MELTTFY : MonoBehaviour {
+    private bool beenSeen = false;
+
+
+    void OnTriggerEnter(Collider interactor) {
+        if (interactor.tag == "Player") {
+            print("YOU BEEN SEEN, HOLY FUCK GET GOOD YOU PIECE OF SHIT!");
+            Time.timeScale = 0;
+        }
+    }
+
+}
