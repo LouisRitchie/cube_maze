ff83b8cb278be6d6fe50fccfe31b61127eb45be5
twistedburger <twistedburger@hotmail.com>
2017-03-11 11:11:23 -0800

    something

--- Assets/Scripts/MELTTFY.cs
+++ Assets/Scripts/MELTTFY.cs
@@ -6,10 +6,27 @@ public class MELTTFY : MonoBehaviour {
     private bool beenSeen = false;
     private bool readyOrNot = false;
     public static bool seen = false;
+    public float radius = 45;
 
     void Start() {
         StartCoroutine(delayStartTime());
     }
+
+    void Update() {
+       /* RaycastHit hit;
+        Debug.DrawRay(transform.position, transform.forward,Color.cyan);
+        if (Physics.SphereCast(transform.position, radius, transform.forward , out hit)) {
+            print(hit.collider.tag);
+            if(hit.collider.tag == "Player") {
+                print("YOU BEEN SEEN, HOLY FUCK GET GOOD YOU PIECE OF SHIT!");
+            }
+
+        }*/
+
+    }
+
+
+
 
     void OnTriggerEnter(Collider interactor) {
         if (interactor.tag == "Player" && readyOrNot) {
