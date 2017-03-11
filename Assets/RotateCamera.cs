930e6eeba8147ea399b41206a458deecacb7eb90
gregolopogus <gregoryperry627@gmail.com>
2017-03-11 14:31:09 -0800

    Added Menu Scene
    Added menu scene and deleted volumetric lighting cause we dont need it.

---
+++ Assets/RotateCamera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour {
+    public Vector3 vector = new Vector3(-1200, 50, 700);
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+        transform.RotateAround(vector, Vector3.up, 30 * Time.deltaTime);
+    }
+}
