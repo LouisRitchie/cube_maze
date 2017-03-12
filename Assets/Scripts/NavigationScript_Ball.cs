eb643c4a823702b4af2b63e91892256fa2d1a0b4
OdieIrony <Jordan_W@live.ca>
2017-03-11 18:43:45 -0800

    Robot tune-ups, music semi-fix
    Fixed how the robots would run through the skinny alleyways by re-baking
    the mesh with a higher agent radius.
    
    Added robot rotation for when the player is not spotted.
    
    Checked if the music wasn't already playing before switching the song.
    STILL BUGGED. MELTTFY scripts change the music independently of robot
    script, this is probably what is causing the bug. TODO: push all music
    activity onto the music object.

--- Assets/Scripts/NavigationScript_Ball.cs
+++ Assets/Scripts/NavigationScript_Ball.cs
@@ -3,12 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-/*  NavigationScript_Ball:
- *      Currently: makes the associated entity traverse a navmesh toward the GameObject "target"
- *      
- *      Lines tagged with "*****" can be uncommented in order to make a raycast trigger this
- *      behaviour.
-*/
+
 public class NavigationScript_Ball : MonoBehaviour {
     //The navigation AI tutorial I was watching: https://www.youtube.com/watch?v=akzsUN4yy1k
     public GameObject playerPos;
@@ -32,45 +27,46 @@ public class NavigationScript_Ball : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        //The current problem with this condition seems to be the sphere moving in such a way
-        //that the camera (meaning the raycast as well) captures the wrong field of view.
-        //Changing the behaviour of the camera to something like an X-Z plane rotation should fix this.
         if (Physics.SphereCast(transform.position, 2f, transform.forward, out hit)) {
             if (hit.collider.gameObject.tag == "Player") {
                 wasSeen = true;
             }
         }
 
-        if (wasSeen) {
-            rayCastDirection  = playerPos.transform.position - transform.position;
-            if (Physics.Raycast(transform.position, rayCastDirection, out chasing)) {
-                if (chasing.collider.gameObject.tag == "Player") {
+        if (wasSeen)
+        {
+            rayCastDirection = playerPos.transform.position - transform.position;
+            if (Physics.Raycast(transform.position, rayCastDirection, out chasing))
+            {
+                if (chasing.collider.gameObject.tag == "Player")
+                {
                     gameObject.GetComponent<NavMeshAgent>().SetDestination(playerPos.transform.position);
                     eyeLight.SetActive(true);
-                    oldSong.Stop();
-                    epicSong.Play();
+                    if (!epicSong.isPlaying)
+                    {
+                        oldSong.Stop();
+                        epicSong.Play();
+                    }
                 }
-                if (chasing.collider.gameObject.tag != "Player" && !MELTTFY.seen) {
+                if (chasing.collider.gameObject.tag != "Player" && !MELTTFY.seen)
+                {
                     gameObject.GetComponent<NavMeshAgent>().SetDestination(startPos);
-                    transform.rotation =startRot;
+                    transform.rotation = startRot;
                     rayCastDirection = transform.forward;
                     eyeLight.SetActive(false);
-                    epicSong.Stop();
-                    oldSong.Play();
+                    if (!oldSong.isPlaying)
+                    {
+                        epicSong.Stop();
+                        oldSong.Play();
+                    }
                     wasSeen = false;
                 }
             }
-            //*****
-            /* else
-            {
-                //TODO: replace following the ball has stopped and is aimlessly rolling into a 
-                //stopped behaviour with random crawling?
-
-                //Currently, this script just makes the ball roll to one direction constantly.
-                //The Vector3.zero vector is probably the problem.
-                //gameObject.GetComponent<NavMeshAgent>().SetDestination(Vector3.zero);
-            }
-            */
+        }
+        else
+        {
+            //Makes the robot spin about the y axis.
+            transform.Rotate(0, (float) 0.5, 0); 
         }
     }
 
